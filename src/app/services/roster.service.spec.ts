4a3c858b073aa0b9342a376fe60f4d93996ef734
jbarron8 <jbarron8@cs6261-vm15.uwg.westga.edu>
2020-06-23 10:59:35 -0400

    Beginning work on the test cases for null names and empty names entered

--- src/app/services/roster.service.spec.ts
+++ src/app/services/roster.service.spec.ts
@@ -4,23 +4,44 @@ import { RosterService } from './roster.service';
 
 describe('RosterService', () => {
   let service: RosterService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(RosterService);
   });
+	describe('Constructor', () => {
+  		it('should be created', () => {
+    			expect(service).toBeTruthy();
+  		});
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	describe('Duplicate names', () => {
+  		it('should not allow duplicate names', () => {
+    			expect(function () {
+			service.addContestant('Tito');
+    			service.addContestant('Tito');
+    			}).toThrow('player is duplicate');	
+		});
 
-  it('should not allow duplicate names', () => {
-    expect(function () {
-	service.addContestant('Tito');
-    	service.addContestant('Tito');
-    }).toThrow('player is duplicate');
-  });
+  		it('should not allow duplicate names test 2', () => {
+    			expect(function () {
+			service.addContestant('Tom');
+			service.addContestant('Jennifer');
+    			service.addContestant('Tom');
+    			}).toThrow('player is duplicate');
+  		});
+  	});
 
-  
+	describe('Null names', () => {
+		it('should not allow null names', () => {
+		});
+		it('should not allow null names test 2', () => {
+		});
+	});
 
+	describe('Empty names', () => {
+		it('should not allow empty names', () => {
+		});
+		it('should not allow empty names test 2', () => {
+		});
+	});
 });
