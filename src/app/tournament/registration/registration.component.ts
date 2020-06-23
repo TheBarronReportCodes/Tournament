cabd6c52a96cd5a28f3868d06cd9f0802f5863c0
jbarron8 <jbarron8@cs6261-vm21.uwg.westga.edu>
2020-06-23 17:50:10 -0400

    Wrote implementation for registerContestants(). Will run tests to make sure it works soon

--- src/app/tournament/registration/registration.component.ts
+++ src/app/tournament/registration/registration.component.ts
@@ -15,7 +15,18 @@ export class RegistrationComponent implements OnInit {
 	this.players = ['', '', '','', '', '', '', '' ]; 
   }
 
+  /* looks through the players and adds any non-empty player names to the RosterService
+  */
   registerContestants() {
+	if (rosterService.getContestants().length == 2 || rosterService.getContestants().length == 4 || rosterService.getContestants().length == 8) {
+		for (var player in this.players) {
+			if (player.trim() == '') {
+				continue;
+			} 
+			rosterService.addContestant(player);
+		}
+	}
+	throw "Rosters can only be 2, 4, or 8 players";
   }
 
 }
