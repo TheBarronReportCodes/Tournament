526e4f1e2b89673d7581431a48db5e4e7a2a95f5
jbarron8 <jbarron8@cs6261-vm23.uwg.westga.edu>
2020-06-22 20:26:21 -0400

    Changed name of Service. declared getContestants() and addContestant methods

---
+++ src/app/services/roster.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RosterService {
+
+  constructor() { }
+
+  /* returns the list of registered contestants' names an
+  */
+  getContestants() {
+  }
+
+  /* adds a single contestant to the registry
+  */
+  addContestant(player: String) {
+  }
+
+}
