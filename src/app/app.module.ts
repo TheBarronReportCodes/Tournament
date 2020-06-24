11be507eb5a7ff20d4e8f4bc9634267af7c112c1
jbarron8 <jbarron8@cs6261-vm04.uwg.westga.edu>
2020-06-24 00:16:43 -0400

    Beginning work on HTML of registrationComponent. Need to implement input tags. FormsModule imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 
@@ -14,7 +15,8 @@ import { RosterService } from './services/roster.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [RosterService],
   bootstrap: [AppComponent]
