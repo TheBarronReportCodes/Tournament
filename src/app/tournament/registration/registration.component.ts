000a3edf51f85828309d0fe0784a74f4ae671ff9
jbarron8 <jbarron8@cs6261-vm04.uwg.westga.edu>
2020-06-23 23:50:16 -0400

    Refactored code inside registerContestants. Not sure if it will work but moving onto component.html and will come back

--- src/app/tournament/registration/registration.component.ts
+++ src/app/tournament/registration/registration.component.ts
@@ -7,26 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  public players: String[];
+  public players: String[]; 
+  public message;
 
   constructor(private rosterService: RosterService) { }
 
   ngOnInit(): void {
 	this.players = ['', '', '','', '', '', '', '' ]; 
+	this.message = null;
   }
 
   /* looks through the players and adds any non-empty player names to the RosterService
   */
   registerContestants() {
-	if (rosterService.getContestants().length == 2 || rosterService.getContestants().length == 4 || rosterService.getContestants().length == 8) {
-		for (var player in this.players) {
-			if (player.trim() == '') {
-				continue;
-			} 
-			rosterService.addContestant(player);
-		}
+	for (var player in this.players) {
+		if (player.trim() == '') {
+			continue;
+		} 
+		this.rosterService.addContestant(player);
 	}
-	throw "Rosters can only be 2, 4, or 8 players";
+
+	if (this.rosterService.getContestants().length == 2  || this.rosterService.getContestants().length == 4 || this.rosterService.getContestants().length == 8) {
+		this.message = this.rosterService.getContestants().toString();
+	} else {
+		this.message = "Roster can only be 2, 4, or 8 players"
+	}
   }
 
 }
