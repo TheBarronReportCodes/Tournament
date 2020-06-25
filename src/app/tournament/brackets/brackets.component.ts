3cb10f9f1e4f5778d109b6296ce6b434c5b7f943
jbarron8 <jbarron8@cs6261-vm03.uwg.westga.edu>
2020-06-25 13:34:44 -0400

    Working on adding a seperate String array to control the contestants for match one. Need to get names of contestants to appear so radio button can select

--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -8,11 +8,19 @@ import { RosterService } from '../../services/roster.service';
 })
 export class BracketsComponent implements OnInit {
   public players: String[];
+  public message;
+  public matchOne: String[];
 
   constructor(private rosterService: RosterService) { }
 
   ngOnInit(): void {
 	this.players = this.rosterService.getContestants();
+	this.message = null;
+	this.matchOne = [this.players[0], this.players[2]];
+  }
+
+  radioChangeHandler (event: any) {
+	this.message = event.target.value;
   }
 
 }
