b4e647766d7aab5d7f52bd1787ddd2913228b07c
jbarron8 <jbarron8@cs6261-vm02.uwg.westga.edu>
2020-06-26 00:09:42 -0400

    created and tested set winner and get winner methods. Adding a couple additional tests to be sure

--- src/app/model/match.spec.ts
+++ src/app/model/match.spec.ts
@@ -1,7 +1,41 @@
 import { Match } from './match';
 
-describe('Match', () => {
-  it('should create an instance', () => {
-    expect(new Match()).toBeTruthy();
-  });
+describe('MatchModel', () => {
+	describe('Constructor and Initialization', () => {
+  		it('should be truthy', () => {
+			var result = new Match('ash','pikachu');
+    			expect(result).toBeTruthy();;
+  		});
+
+  		it('should return ash as the first contestant', () => {
+			var result = new Match('ash','pikachu');
+    			expect(result.firstContestant).toEqual('ash');
+  		});
+
+  		it('should return pikachu as the second contestant', () => {
+			var result = new Match('ash','pikachu');
+    			expect(result.secondContestant).toEqual('pikachu');
+  		});
+
+  		it('should have empty string as initial winner', () => {
+			var result = new Match('ash','pikachu');
+    			expect(result.getWinner()).toEqual('');
+  		});
+	});
+
+	describe('Set Winner', () => {
+  		it('should set and return winner as ash', () => {
+			var match1 = new Match('ash','gary');
+			match1.setWinner(match1.firstContestant);
+			var result = match1.getWinner();
+    			expect(result).toEqual('ash');
+  		});
+
+		it('should set and return winner as gary', () => {
+			var match1 = new Match('ash','gary');
+			match1.setWinner(match1.secondContestant);
+			var result = match1.getWinner();
+    			expect(result).toEqual('gary');
+  		});
+	});
 });
